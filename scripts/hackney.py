55e964b71a9477e3526b72fd9d240f0eb4f54422
Philip Douglas <psd@pdouglas.net>
2021-02-14 15:53:49 +0000

    Fix Tabula misaligning the columns

--- scripts/hackney.py
+++ scripts/hackney.py
@@ -1,35 +1,3 @@
-# """
-# Hackney's register is public but can only be searched by postcode:
-# https://propertylicensing.hackney.gov.uk/public-register
-# """
-
-# from bs4 import BeautifulSoup
-# import requests
-
-# BASE_URL = "https://propertylicensing.hackney.gov.uk/public-register"
-
-
-# def dump_search(postcode):
-#     response = requests.get(BASE_URL)
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     query_params = {}
-#     search_field = soup.find(id="search_query")["name"]
-#     query_params[search_field] = postcode
-#     csrf_input = soup.find(id="search__token")
-#     query_params[csrf_input["name"]] = csrf_input["value"]
-
-#     response = requests.get(BASE_URL, params=query_params)
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     print(response.text)
-
-
-
-# def dump_all():
-#     postcodes = (f"E{i}" for i in range(1, 16))
-#     data = []
-#     for postcode in postcodes:
-#         data += dump_search(postcode)
-
 """
 Hackney host a PDF of their property licenses on google drive, linked from a page on their webite.
 The PDF contains tabular data that we can parse out and convert to a CSV.
@@ -46,6 +14,32 @@ import tabula
 START_URL = "https://hackney.gov.uk/hmo"
 DRIVE_DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id={}"
 
+# Some magic numbers to help Tabula find the columns, without which it can't
+# cope with all the whitespace.
+COLUMNS = [
+    79,
+    164,
+    220,
+    294.525,
+    325.215,
+    340.065,
+    354.915,
+    369,
+    402,
+    423.225,
+    451.935,
+    478.665,
+    504.405,
+    534.105,
+    553.905,
+    574,
+    610,
+    632.115,
+    652.905,
+    675.675,
+    695.475,
+    714.285,
+]
 
 def dump():
     # Get the latest google drive link from hackney.gov.uk in case it changes when they update
@@ -56,9 +50,7 @@ def dump():
     drive_token = path.split("/")[3]
     drive_url = DRIVE_DOWNLOAD_URL.format(drive_token)
 
-    dataframe = tabula.convert_into(drive_url, "hackney.csv", output_format="csv", pages="all")
-
-
+    tabula.convert_into(drive_url, "hackney.csv", output_format="csv", pages="all", guess=False, stream=True, columns=COLUMNS)
 
 
 if __name__ == "__main__":
