30d2f382c3ec1c449603fdb940e8eba9d16bfb37
Philip Douglas <psd@pdouglas.net>
2021-02-14 16:20:33 +0000

    Tidy up the CSV headings

--- scripts/hackney.py
+++ scripts/hackney.py
@@ -8,6 +8,7 @@ Note: requires a Java runtime
 from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
+import pandas
 import requests
 import tabula
 
@@ -40,6 +41,31 @@ COLUMNS = [
     695.475,
     714.285,
 ]
+HEADINGS = [
+    "Licence No.",
+    "Property Address",
+    "Licence Holder Name",
+    "Licence Holder Address (business only)",
+    "Property Type",
+    "No. of Storeys",
+    "No. of Storeys Front",
+    "No. of Storeys Read",
+    "Accommodation Type",
+    "No. of Kitchen",
+    "No. of rooms providing sleeping accommodation",
+    "No. of Toilets and Wash basins",
+    "No. of rooms providing living accommodation",
+    "No. of Bathrooms/Shower rooms",
+    "Self Contained Units",
+    "Non-Self Contained Units",
+    "Licence Type ",
+    "Licence Expiry Date",
+    "Licence Start Date",
+    "No. of Households",
+    "No. of Occupants",
+    "Max. Occupants",
+    "LICENCE CONDITIONS",
+]
 
 def dump():
     # Get the latest google drive link from hackney.gov.uk in case it changes when they update
@@ -50,7 +76,14 @@ def dump():
     drive_token = path.split("/")[3]
     drive_url = DRIVE_DOWNLOAD_URL.format(drive_token)
 
-    tabula.convert_into(drive_url, "hackney.csv", output_format="csv", pages="all", guess=False, stream=True, columns=COLUMNS)
+    dataframes = tabula.read_pdf(drive_url, pages="all", guess=False, stream=True, columns=COLUMNS)
+
+    # Get rid of the badly parsed heading wrap rows
+    dataframes = [frame[2:] for frame in dataframes]
+    frame = pandas.concat(dataframes)
+    frame.columns = HEADINGS
+
+    frame.to_csv("data/hackney.csv")
 
 
 if __name__ == "__main__":
